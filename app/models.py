3f7ac5fa7f3d6d7445f14f8f9debdc73ecd8bc91
Vuatly <kudoyavtsev@bk.ru>
2020-02-02 16:20:26 +0300

    Add model Car

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    manufacturer = models.CharField(max_length=25)
+    model = models.CharField(max_length=25)
+    release_year = models.IntegerField()
+
+    TRANSMISSION_CHOICES = [(1, 'Mechanics'), 
+                            (2, 'Automatic'), 
+                            (3, 'Robot')]
+
+    transmission = models.IntegerField(choices=TRANSMISSION_CHOICES)
+    color = models.CharField(max_length=25)
