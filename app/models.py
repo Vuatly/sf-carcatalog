ff8d131b70e5abcbb5b01b37fa29e88338ec8d2f
Vuatly <kudoyavtsev@bk.ru>
2020-02-03 14:40:27 +0300

    Test SearchVector

--- app/models.py
+++ app/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
+
 class Car(models.Model):
     manufacturer = models.CharField(max_length=25)
     model = models.CharField(max_length=25)
     release_year = models.IntegerField()
 
-    TRANSMISSION_CHOICES = [(1, 'Mechanics'), 
-                            (2, 'Automatic'), 
+    TRANSMISSION_CHOICES = [(1, 'Mechanics'),
+                            (2, 'Automatic'),
                             (3, 'Robot')]
 
     transmission = models.IntegerField(choices=TRANSMISSION_CHOICES)
     color = models.CharField(max_length=25)
 
     def __str__(self):
-        return self.manufacturer + ' ' + self.model
-    
+        return self.manufacturer + ' ' + self.model + ' ' + str(self.release_year)
--- app/views.py
+++ app/views.py
@@ -1,7 +1,31 @@
-from .models import Car
+from django.contrib.postgres.search import SearchVector
+from django.db.models import Q
 from django.views.generic import ListView
+from .models import Car
+
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 class CarsList(ListView):
     model = Car
     template_name = 'cars-list.html'
     context_object_name = 'cars'
+
+    def get_queryset(self):
+        logger.debug(f"Request to CarsList from user: {self.request.user.id} with params: {self.request.GET}")
+        logger.info("CarsList.get_queryset called")
+        try:
+            search_query = self.request.GET.get('search', '')
+            qs = Car.objects.all()
+            if search_query:
+                qs = Car.objects.annotate(
+                    search=SearchVector('model', 'manufacturer', 'color', 'release_year', 'transmission'),
+                ).filter(search=search_query)
+                if not qs:
+                    logger.warning("Cars list is empty")
+            return qs
+        except Exception as exc:
+            logger.error(str(exc))
+            raise
